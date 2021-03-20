9a06367df5847ce6cfdd05f6f57f8c3d685ccffd
jairad26 <jairad26@gmail.com>
2021-03-19 17:47:57 -0700

    set up place search API for certain region

---
+++ main.py
@@ -0,0 +1,40 @@
+import googlemaps
+import uuid
+from types import GeneratorType
+import responses
+from . import TestCase
+
+
+class PlacesTest(TestCase):
+    def setUp(self):
+        self.key = "AIzaSyDJZMEwUkoKPrBqgvF54T9cnwBEqTP4Pzs"
+        self.client = googlemaps.Client(self.key)
+        self.location = (33.696462, -117.798897)
+        self.type = "restaurant"
+        self.language = "en"
+        self.region = "US"
+        self.radius = 1000
+        
+    @responses.activate
+    def test_search_places_nearby(self):
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+        responses.add(
+            responses.GET,
+            url,
+            body = '{"status": "OK", "results": [], "html_attributions":[]}',
+            status = 200,
+            content_type = "application/json"
+        )
+        
+        self.client.places_nearby(
+            location = self.location,
+            language = self.language,
+            rank_by = "distance",
+            type = self.type
+            )
+        
+        self.assertEqual(1,len(responses.calls))
+        self.assertURLEqual(
+            "%s?language=en&location=33.696462%%2C-117.798897&rankby=distance&type=restaurant&key=%s" %(url, self.key),
+            responses.calls[0].request.url,
+        )
